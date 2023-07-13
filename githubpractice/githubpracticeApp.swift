17c5614a5c270a4dc3e33adcdecc42f0e3b241f4
Saana Shah <the.real.saana@gmail.com>
2023-07-13 14:15:18 -0700

    Initial Commit

---
+++ githubpractice/githubpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubpracticeApp.swift
+//  githubpractice
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct githubpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
